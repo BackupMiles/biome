1740f7086b2394c7d09fb11dbdcaf404e55d43cb
BackupMiles <leanwithmiles@gmail.com>
2024-06-22 12:08:13 +0200

    feat: add search tag

--- crates/biome_cli/src/execute/diagnostics.rs
+++ crates/biome_cli/src/execute/diagnostics.rs
@@ -104,7 +104,7 @@ pub(crate) struct UnhandledDiagnostic;
 pub(crate) struct SkippedDiagnostic;
 
 #[derive(Debug, Diagnostic)]
-#[diagnostic(category = "search")]
+#[diagnostic(category = "search", tags(SEARCH))]
 pub(crate) struct SearchDiagnostic;
 
 /// Extension trait for turning [Display]-able error types into [TraversalError]
