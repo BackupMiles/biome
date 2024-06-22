a89b433a43d9fa1b21cf296b06adf791747f7c3b
BackupMiles <leanwithmiles@gmail.com>
2024-06-22 12:08:48 +0200

    ref(search): change severity to hint

--- crates/biome_cli/src/execute/process_file/search.rs
+++ crates/biome_cli/src/execute/process_file/search.rs
@@ -39,7 +39,7 @@ pub(crate) fn search_with_guard<'ctx>(
                 diagnostics: result
                     .matches
                     .into_iter()
-                    .map(|mat| SearchDiagnostic.with_file_span(mat).with_severity(Severity::Information).into())
+                    .map(|mat| SearchDiagnostic.with_file_span(mat).with_severity(Severity::Hint).into())
                     .collect(),
                 skipped_diagnostics: 0,
             };
